c77f3fa695de244bae1a1f8113b8017663d49167
Lakshmikanth <k.lakshmikanth002@gmail.com>
2023-12-08 18:44:34 +0530

    Update test.py

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -1,8 +1,8 @@
 import os
 
-print(__file__)
-print(os.path.abspath(__file__))
-print(os.path.dirname(os.path.abspath(__file__)))
+print("__file__ :\n",__file__)
+print("os.path.abspath(__file__) :\n",os.path.abspath(__file__))
+print("os.path.dirname(os.path.abspath(__file__)) :\n",os.path.dirname(os.path.abspath(__file__)))
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
-print(f"{dir_path}/../../conf")
+print(f"dir_path : \n{dir_path}/../../conf")
